fc67e8185d4a92041cba47f2f82f1d5b4d953b49
Daniel Gallardo <daniel.gallardo@flatironschool.com>
2014-05-20 12:21:45 -0400

    refactor of question 1

--- projectEuler/ViewController.m
+++ projectEuler/ViewController.m
@@ -36,14 +36,8 @@
     NSInteger sum =0;
     
     for (NSInteger i=0; i<[number integerValue]; i++) {
-        if (i % 15 == 0) {
-            sum +=i;
-        }
-        else if (i % 3 == 0){
-            sum +=i;
-        }
-        else if (i % 5 == 0){
-            sum +=i;
+        if (i % 3 == 0 || i % 5 ==0) {
+            sum += i;
         }
     }
     
